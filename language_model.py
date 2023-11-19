541ef4effb541eb2d1b4b66d5d014927f7ce82b4
Pegah Ramezani <pegah.ramezani1996@gmail.com>
2023-11-19 09:23:29 +0100

    add language model

---
+++ language_model.py
@@ -0,0 +1,23 @@
+
+import fasttext.util
+import numpy as np
+#fasttext.util.download_model('ar', if_exists='ignore')
+ArabicWE =fasttext.load_model('cc.ar.300.bin')
+def ArabicWEFunction(Word):
+    return [i for i in ArabicWE[Word]]
+       
+def SimilarWords(Word):
+    if Word in ArabicWE.get_words():
+        words_difference = {}
+        for each_word in ArabicWE.get_words()[:10000]:
+            words_difference[each_word] = sum(
+                [abs(float(i) - float(j)) for i, j in zip([i for i in ArabicWE[Word]], [i for i in ArabicWE[each_word]])]
+            )
+            #
+        sorted_words = sorted(words_difference.items(), key=lambda item: item[1])
+        #out=sorted_words
+        out= [x[0] for x in sorted_words]
+        return out[1:6]
+    else:
+        return [('Word not found', 0.0)]  # Return a tuple indicating that the word was not found
+    
--- web_application.py
+++ web_application.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-
+from language_model import SimilarWords
 app = Flask(__name__)
 
 
@@ -9,15 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
 @app.route('/search/', methods=['POST'])
 def search_word():
     # database query needs to go here
     # It currently only prints the input
-    print(request.form['word'])
-    output = ['hi', 'mom', '!']
+    word=request.form['word']
+    output=SimilarWords (word)
     return render_template("index.html", output=output)
-
 
 if __name__ == '__main__':
     app.run()
