1600f6df8a632a6c94e2d600af4b240e2a0ea4a5
Philipp Bender <philipp.bender@hetzner.com>
2023-11-19 09:00:30 +0100

    Add secret key as build arg and Improve form validation
    Updated GitHub Actions workflow file to include a new build argument - SECRET_KEY in the Docker build step. This eliminates the need to expose sensitive information during application runtime.
    
    Also included is the addition of form validation via JavaScript alert on the front end and WTForms on the backend. The /search route, now modified to POST only, validates the form on submission.
    
    This commit includes necessary changes in the requirements.txt file and web_application.py where wtforms are added and employed for data validation respectively.
    
    Lastly, the form in index.html has been transformed to use wtforms ensuring a consistent application of form validation on both the client and the server side.

--- web_application.py
+++ web_application.py
@@ -1,22 +1,38 @@
 from flask import Flask
 from flask import render_template
-from flask import request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import os
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+
+class SubmitForm(FlaskForm):
+    word = StringField('Word', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    form = SubmitForm()
+    return render_template("index.html", form=form)
 
 
 @app.route('/search', methods=['POST'])
 def search_word():
+    form = SubmitForm()
+    output = None
+
+    if form.validate_on_submit():
+        print(form.word.data)
+        output = ['hi', 'mom', '!']
     # database query needs to go here
     # It currently only prints the input
-    print(request.form['word'])
-    output = ['hi', 'mom', '!']
-    return render_template("index.html", output=output)
+    # print(request.form['word'])
+    # output = ['hi', 'mom', '!']
+    return render_template("index.html", form=form, output=output)
 
 
 if __name__ == '__main__':
